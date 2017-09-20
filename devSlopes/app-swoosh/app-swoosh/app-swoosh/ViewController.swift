2f7e0b65e8b26f368ec0d0cc6804ef63178a6ea6
Gabriel Abraham <gabrielabraham@Gabriels-Mac-mini.local>
2017-09-20 10:46:53 +0100

    segue

--- devSlopes/app-swoosh/app-swoosh/app-swoosh/ViewController.swift
+++ devSlopes/app-swoosh/app-swoosh/app-swoosh/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+      
     }
 
+    @IBAction func unwindFromSkillVC(unwindSeuge: UIStoryboardSegue){
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
